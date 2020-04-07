319af0e7339d9d2b1941b363b5159cb64e3959d8
Andrew Kelley <andrew@ziglang.org>
2020-04-07 15:14:16 -0400

    update for latest zig

--- main.zig
+++ main.zig
@@ -11,9 +11,9 @@ pub const Inst = union(enum) {
     div: i8,
 };
 
-fn push_string(strings: []u8, index: *usize, string: [] const u8) usize {
+fn push_string(strings: []u8, index: *usize, string: []const u8) usize {
     const result = index.*;
-    mem.copy(u8, strings[index.* ..], string);
+    mem.copy(u8, strings[index.*..], string);
     index.* += string.len;
     strings[index.*] = 0;
     index.* += 1;
@@ -27,7 +27,7 @@ pub fn main() anyerror!void {
 
     var it = mem.tokenize(args[1], " \t\n\r");
     const first_operand = it.next() orelse return error.ParseError;
-    try list.append(Inst{ .add = try std.fmt.parseInt(i8, first_operand, 10)});
+    try list.append(Inst{ .add = try std.fmt.parseInt(i8, first_operand, 10) });
     while (true) {
         const operator = it.next() orelse break;
         const operand = it.next() orelse return error.ParseError;
@@ -40,13 +40,12 @@ pub fn main() anyerror!void {
     }
     const instructions = list.toOwnedSlice();
 
-
     const entry_addr = 0x1337000;
     const target = std.Target.current;
     const ptr_width: enum {
         _32,
         _64,
-    } = switch (target.getArchPtrBitWidth()) {
+    } = switch (target.cpu.arch.ptrBitWidth()) {
         32 => ._32,
         64 => ._64,
         else => return error.UnsupportedArchitecture,
@@ -60,7 +59,7 @@ pub fn main() anyerror!void {
     const text_index = push_string(&string_buf, &string_offset, ".text");
     const start_index = push_string(&string_buf, &string_offset, "_start");
 
-    const endian = target.getArch().endian();
+    const endian = target.cpu.arch.endian();
     const end_of_program_header_offset = @sizeOf(elf.Elf64_Ehdr) + @sizeOf(elf.Elf64_Phdr);
     const sections = 4;
     var hdr_buf: [end_of_program_header_offset + @sizeOf(elf.Elf64_Shdr) * sections]u8 = undefined;
@@ -92,16 +91,15 @@ pub fn main() anyerror!void {
 
     assert(index == 16);
 
-    // TODO: https://github.com/ziglang/zig/issues/863 makes this (and all following) @ptrCast unnecessary
-    mem.writeInt(u16, @ptrCast(*[2]u8, &hdr_buf[index]), @enumToInt(elf.ET.EXEC), endian);
+    mem.writeInt(u16, hdr_buf[index..][0..2], @enumToInt(elf.ET.EXEC), endian);
     index += 2;
 
-    const machine = target.getArch().toElfMachine();
-    mem.writeInt(u16, @ptrCast(*[2]u8, &hdr_buf[index]), @enumToInt(machine), endian);
+    const machine = target.cpu.arch.toElfMachine();
+    mem.writeInt(u16, hdr_buf[index..][0..2], @enumToInt(machine), endian);
     index += 2;
 
     // ELF Version, again
-    mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), 1, endian);
+    mem.writeInt(u32, hdr_buf[index..][0..4], 1, endian);
     index += 4;
 
     switch (ptr_width) {
@@ -110,54 +108,54 @@ pub fn main() anyerror!void {
         },
         ._64 => {
             // e_entry
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), entry_addr, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], entry_addr, endian);
             index += 8;
 
             // e_phoff
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), @sizeOf(elf.Elf64_Ehdr), endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], @sizeOf(elf.Elf64_Ehdr), endian);
             index += 8;
 
             // e_shoff
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), end_of_program_header_offset, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], end_of_program_header_offset, endian);
             index += 8;
         },
     }
 
     const e_flags = 0;
-    mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), e_flags, endian);
+    mem.writeInt(u32, hdr_buf[index..][0..4], e_flags, endian);
     index += 4;
 
     const e_ehsize: u16 = switch (ptr_width) {
         ._32 => @sizeOf(elf.Elf32_Ehdr),
         ._64 => @sizeOf(elf.Elf64_Ehdr),
     };
-    mem.writeInt(u16, @ptrCast(*[2]u8, &hdr_buf[index]), e_ehsize, endian);
+    mem.writeInt(u16, hdr_buf[index..][0..2], e_ehsize, endian);
     index += 2;
 
     const e_phentsize: u16 = switch (ptr_width) {
         ._32 => @sizeOf(elf.Elf32_Phdr),
         ._64 => @sizeOf(elf.Elf64_Phdr),
     };
-    mem.writeInt(u16, @ptrCast(*[2]u8, &hdr_buf[index]), e_phentsize, endian);
+    mem.writeInt(u16, hdr_buf[index..][0..2], e_phentsize, endian);
     index += 2;
 
     const e_phnum = 1;
-    mem.writeInt(u16, @ptrCast(*[2]u8, &hdr_buf[index]), e_phnum, endian);
+    mem.writeInt(u16, hdr_buf[index..][0..2], e_phnum, endian);
     index += 2;
 
     const e_shentsize: u16 = switch (ptr_width) {
         ._32 => @sizeOf(elf.Elf32_Shdr),
         ._64 => @sizeOf(elf.Elf64_Shdr),
     };
-    mem.writeInt(u16, @ptrCast(*[2]u8, &hdr_buf[index]), e_shentsize, endian);
+    mem.writeInt(u16, hdr_buf[index..][0..2], e_shentsize, endian);
     index += 2;
 
     const e_shnum = sections;
-    mem.writeInt(u16, @ptrCast(*[2]u8, &hdr_buf[index]), e_shnum, endian);
+    mem.writeInt(u16, hdr_buf[index..][0..2], e_shnum, endian);
     index += 2;
 
     const e_shstrndx = 1;
-    mem.writeInt(u16, @ptrCast(*[2]u8, &hdr_buf[index]), e_shstrndx, endian);
+    mem.writeInt(u16, hdr_buf[index..][0..2], e_shstrndx, endian);
     index += 2;
 
     assert(index == e_ehsize);
@@ -165,35 +163,34 @@ pub fn main() anyerror!void {
     // Program header
 
     const p_type = elf.PT_LOAD;
-    mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), p_type, endian);
+    mem.writeInt(u32, hdr_buf[index..][0..4], p_type, endian);
     index += 4;
 
-    var machine_code = try std.Buffer.init(std.heap.page_allocator, "");
+    var machine_code = std.ArrayList(u8).init(std.heap.page_allocator);
 
     // xor edi, edi
-    try machine_code.append(&[_]u8{
+    try machine_code.appendSlice(&[_]u8{
         0x31, 0xff,
     });
 
     // add edi, 123
     for (instructions) |item| {
         switch (item) {
-            .add => |n| try machine_code.append(&[_]u8 {
+            .add => |n| try machine_code.appendSlice(&[_]u8{
                 0x83, 0xc7, @bitCast(u8, n),
             }),
-            .sub => |n| try machine_code.append(&[_]u8 {
+            .sub => |n| try machine_code.appendSlice(&[_]u8{
                 0x83, 0xef, @bitCast(u8, n),
             }),
-            .mul => |n| try machine_code.append(&[_]u8 {
+            .mul => |n| try machine_code.appendSlice(&[_]u8{
                 0x6b, 0xff, @bitCast(u8, n),
             }),
             .div => |n| {},
         }
     }
 
-
     // exit(edi)
-    try machine_code.append(&[_]u8{
+    try machine_code.appendSlice(&[_]u8{
         0xb8, 0x3c, 0x00, 0x00, 0x00, // mov eax, 0x3c
         0x0f, 0x05, // syscall
     });
@@ -211,32 +208,31 @@ pub fn main() anyerror!void {
     switch (ptr_width) {
         ._32 => @panic("TODO"),
         ._64 => {
-
             const p_flags = elf.PF_X | elf.PF_R;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), p_flags, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], p_flags, endian);
             index += 4;
 
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), p_offset, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], p_offset, endian);
             index += 8;
 
             const p_vaddr = entry_addr;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), p_vaddr, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], p_vaddr, endian);
             index += 8;
 
             const p_paddr = entry_addr;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), p_paddr, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], p_paddr, endian);
             index += 8;
 
-            const p_filesz = machine_code.len();
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), p_filesz, endian);
+            const p_filesz = machine_code.items.len;
+            mem.writeInt(u64, hdr_buf[index..][0..8], p_filesz, endian);
             index += 8;
 
-            const p_memsz = machine_code.len();
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), p_memsz, endian);
+            const p_memsz = machine_code.items.len;
+            mem.writeInt(u64, hdr_buf[index..][0..8], p_memsz, endian);
             index += 8;
 
             const p_align = 0x1000;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), p_align, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], p_align, endian);
             index += 8;
 
             assert(index == end_of_program_header_offset);
@@ -249,186 +245,186 @@ pub fn main() anyerror!void {
         // Null
         {
             const sh_name = 0;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_name, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_name, endian);
             index += 4;
 
             const sh_type = elf.SHT_NULL;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_type, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_type, endian);
             index += 4;
 
             const sh_flags = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_flags, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_flags, endian);
             index += 8;
 
             const sh_addr = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_addr, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_addr, endian);
             index += 8;
 
             const sh_offset = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_offset, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_offset, endian);
             index += 8;
 
             const sh_size = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_size, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_size, endian);
             index += 8;
 
             const sh_link = 0;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_link, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_link, endian);
             index += 4;
 
             const sh_info = 0;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_info, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_info, endian);
             index += 4;
 
             const sh_align = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_align, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_align, endian);
             index += 8;
 
             const sh_entsize = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_entsize, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_entsize, endian);
             index += 8;
         }
 
         // Strings
         {
             const sh_name = @intCast(u32, strtab_index);
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_name, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_name, endian);
             index += 4;
 
             const sh_type = elf.SHT_STRTAB;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_type, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_type, endian);
             index += 4;
 
             const sh_flags = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_flags, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_flags, endian);
             index += 8;
 
             const sh_addr = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_addr, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_addr, endian);
             index += 8;
 
             const sh_offset = hdr_buf.len;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_offset, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_offset, endian);
             index += 8;
 
             const sh_size = string_offset;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_size, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_size, endian);
             index += 8;
 
             const sh_link = 0;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_link, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_link, endian);
             index += 4;
 
             const sh_info = 0;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_info, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_info, endian);
             index += 4;
 
             const sh_align = 1;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_align, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_align, endian);
             index += 8;
 
             const sh_entsize = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_entsize, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_entsize, endian);
             index += 8;
         }
 
         // Text
         {
             const sh_name = @intCast(u32, text_index);
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_name, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_name, endian);
             index += 4;
 
             const sh_type = elf.SHT_PROGBITS;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_type, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_type, endian);
             index += 4;
 
             const sh_flags = elf.SHF_ALLOC | elf.SHF_EXECINSTR;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_flags, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_flags, endian);
             index += 8;
 
             const sh_addr = entry_addr;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_addr, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_addr, endian);
             index += 8;
 
             const sh_offset = p_offset;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_offset, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_offset, endian);
             index += 8;
 
-            const sh_size = machine_code.len();
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_size, endian);
+            const sh_size = machine_code.items.len;
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_size, endian);
             index += 8;
 
             const sh_link = 0;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_link, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_link, endian);
             index += 4;
 
             const sh_info = 0;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_info, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_info, endian);
             index += 4;
 
             const sh_align = 0x1000;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_align, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_align, endian);
             index += 8;
 
             const sh_entsize = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_entsize, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_entsize, endian);
             index += 8;
         }
 
         // Symbol table
         {
             const sh_name = @intCast(u32, symtab_index);
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_name, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_name, endian);
             index += 4;
 
             const sh_type = elf.SHT_SYMTAB;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_type, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_type, endian);
             index += 4;
 
             const sh_flags = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_flags, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_flags, endian);
             index += 8;
 
             const sh_addr = 0;
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_addr, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_addr, endian);
             index += 8;
 
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), symbol_table_sh_offset, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], symbol_table_sh_offset, endian);
             index += 8;
 
             const sh_size = symbol_entries * @sizeOf(elf.Elf64_Sym);
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_size, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_size, endian);
             index += 8;
 
             const sh_link = 1;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_link, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_link, endian);
             index += 4;
 
             const sh_info = 2;
-            mem.writeInt(u32, @ptrCast(*[4]u8, &hdr_buf[index]), sh_info, endian);
+            mem.writeInt(u32, hdr_buf[index..][0..4], sh_info, endian);
             index += 4;
 
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), symbol_table_sh_align, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], symbol_table_sh_align, endian);
             index += 8;
 
             const sh_entsize = @sizeOf(elf.Elf64_Sym);
-            mem.writeInt(u64, @ptrCast(*[8]u8, &hdr_buf[index]), sh_entsize, endian);
+            mem.writeInt(u64, hdr_buf[index..][0..8], sh_entsize, endian);
             index += 8;
         }
     }
 
     assert(index == hdr_buf.len);
 
-    const file = try std.fs.File.openWriteMode("output", 0o755);
+    const file = try std.fs.cwd().createFile("output", .{ .mode = 0o755 });
     defer file.close();
 
-    const out = &file.outStream().stream;
+    const out = file.outStream();
 
-    try file.write(hdr_buf[0..index]);
-    try file.write(string_buf[0..string_offset]);
+    try file.writeAll(hdr_buf[0..index]);
+    try file.writeAll(string_buf[0..string_offset]);
     assert(symbol_entries == 2);
-    try file.write(zeroes[0..symbol_table_padding]);
-    try writeStruct(out, elf.Elf64_Sym, elf.Elf64_Sym {
+    try file.writeAll(zeroes[0..symbol_table_padding]);
+    try writeStruct(out, elf.Elf64_Sym, elf.Elf64_Sym{
         .st_name = 0,
         .st_info = 0,
         .st_other = 0,
@@ -436,19 +432,19 @@ pub fn main() anyerror!void {
         .st_value = 0,
         .st_size = 0,
     });
-    try writeStruct(out, elf.Elf64_Sym, elf.Elf64_Sym {
+    try writeStruct(out, elf.Elf64_Sym, elf.Elf64_Sym{
         .st_name = @intCast(u32, start_index),
         .st_info = (elf.STB_LOCAL << 4) | elf.STT_FUNC,
         .st_other = 0,
         .st_shndx = 2,
         .st_value = entry_addr,
-        .st_size = machine_code.len(),
+        .st_size = machine_code.items.len,
     });
-    try file.write(zeroes[0..pad]);
-    try file.write(machine_code.toSlice());
+    try file.writeAll(zeroes[0..pad]);
+    try file.writeAll(machine_code.toSlice());
 }
 
 pub fn writeStruct(self: var, comptime T: type, value: T) !void {
     comptime assert(@typeInfo(T).Struct.layout != .Auto);
-    return self.writeFn(self, @ptrCast([*] const u8, &value)[0..@sizeOf(T)]);
+    return self.writeAll(@ptrCast([*]const u8, &value)[0..@sizeOf(T)]);
 }
